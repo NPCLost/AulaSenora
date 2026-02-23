c4822d3c5b23ea7aae904a07e7fe67c2f1f89b02
Juan <juaneslozano68@gmail.com>
2026-02-22 20:03:43 -0500

    Primer Commit

---
+++ backend/src/test/java/AulaSenora/AulaSenoraApplicationTests.java
@@ -0,0 +1,13 @@
+package AulaSenora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaSenoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
